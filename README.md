061ab92b2537021e151be86235242bbe55ab7093
Tim Nosov <timnosov@gmail.com>
2016-12-29 13:04:02 -0800

    more updates. Sorry.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # website-hugo
 The source files for the club's website
+
+
+cd sfusat_hougo
+hugo server
--- themes/hugo-elate-theme/static/js/google_map.js
+++ themes/hugo-elate-theme/static/js/google_map.js
@@ -5,13 +5,13 @@ function init() {
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
-    var myLatlng = new google.maps.LatLng(51.0825523,3.574402,11);
+    var myLatlng = new google.maps.LatLng(49.277411, -122.914161);
     // 39.399872
     // -8.224454
     
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
-        zoom: 7,
+        zoom: 8,
 
         // The latitude and longitude to center the map (always required)
         center: myLatlng,
@@ -30,12 +30,12 @@ function init() {
     // Create the Google Map using out element and options defined above
     var map = new google.maps.Map(mapElement, mapOptions);
     
-    var addresses = ['Ghent'];
+    var addresses = ['vancouver'];
 
     for (var x = 0; x < addresses.length; x++) {
         $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
             var p = data.results[0].geometry.location
-            var latlng = new google.maps.LatLng(p.lat, p.lng);
+            var latlng = new google.maps.LatLng(49.277411, -122.914161);
             new google.maps.Marker({
                 position: latlng,
                 map: map,
