3fae79cd1c8384a708b4979cc1415ad433c56ccc
Steven Huang <stevenhuang1995@gmail.com>
2017-04-15 04:57:03 -0700

    Use relative paths, fix ghpages subdirectory

--- publish_to_ghpages.sh
+++ publish_to_ghpages.sh
@@ -19,7 +19,7 @@ echo "Removing existing files"
 rm -rf public/*
 
 echo "Generating site"
-hugo
+env HUGO_BASEURL="https://sfusatclub.github.io/website-hugo/" hugo
 
 echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)" && git push
